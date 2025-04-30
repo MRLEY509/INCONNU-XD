86d05bf8c4e6717fc7b4189987a24cdf372ba464
ᴘʀᴏғᴇssᴇᴜʀ-xᴅ <ley376171@gmail.com>
2025-04-29 20:08:36 -0400

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -6,8 +6,8 @@ const stickerCommand = async (m, gss) => {
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
   const text = m.body.slice(prefix.length + cmd.length).trim();
 
-  const packname = global.packname || "INCONNU-XD";
-  const author = global.author || "🖤🇵🇰";
+  const packname = global.packname || "༺𒋲✘🔮𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 𝐓𝐓𝟐𝟐.𝟎🩸✘❶𒋲";
+  const author = global.author || "☪";
 
   const validCommands = ['sticker', 's', 'autosticker'];
 
