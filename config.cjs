947d010db73c8bdc9f71dfe4a31efc1e23f63e35
ᴘʀᴏғᴇssᴇᴜʀ-xᴅ <ley376171@gmail.com>
2025-04-29 20:16:58 -0400

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -24,7 +24,7 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "private",
-  BOT_NAME: process.env.BOT_NAME || "RECTANGLE-XD",
+  BOT_NAME: process.env.BOT_NAME || "PROFESSEUR-XD",
   MENU_IMAGE: process.env.MENU_IMAGE || "https://files.catbox.moe/umpjhb.jpg",
   DESCRIPTION: process.env.DESCRIPTION || "WELCOME TO PROFESSEUR-XD",
   OWNER_NAME: process.env.OWNER_NAME || "PROFESSEUR-XD",
